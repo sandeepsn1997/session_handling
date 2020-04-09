efea39f1e2c071ede42f59d850b4066785c754ea
sandeepsn1997 <sandeepsn1997@gmail.com>
2020-04-10 00:22:08 +0530

    installing package and creation of components

--- sessionPoc/src/app/app.component.ts
+++ sessionPoc/src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sessionPoc';
+
+
+
+  constructor(private userIdle: UserIdleService) {
+  }
+
+  ngOnInit() {
+    //Start watching for user inactivity.
+    this.userIdle.startWatching();
+
+    // Start watching when user idle is starting.
+    this.userIdle.onTimerStart().subscribe(count => console.log(count));
+
+    // Start watch when time is up.
+    this.userIdle.onTimeout().subscribe(() => console.log('Time is up!'));
+  }
+
+  stop() {
+    this.userIdle.stopTimer();
+  }
+
+  stopWatching() {
+    this.userIdle.stopWatching();
+  }
+
+  startWatching() {
+    this.userIdle.startWatching();
+  }
+
+  restart() {
+    this.userIdle.resetTimer();
+  }
+
+
+
 }
--- sessionPoc/src/app/app.module.ts
+++ sessionPoc/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserIdleModule } from 'angular-user-idle';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserIdleModule.forRoot({idle: 600, timeout: 300, ping: 120})
   ],
   providers: [],
   bootstrap: [AppComponent]
